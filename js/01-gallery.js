51e7afe1d5f42599632f874f920aafaba579a9cc
Ivan <gubarenkovanek@gmail.com>
2022-09-02 22:09:28 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+const markup = galleryItems.map(({original,preview,description}) =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href = ${original}>
+    <img 
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt= ${description}
+        />
+        </a>
+        </div>`).join("");
+
+
+gallery.innerHTML = markup;
+
+gallery.addEventListener('click',openImg);
+
+function openImg(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    const selectedImg = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${selectedImg}" width="800" height="600">
+`)
+    instance.show();
+
+    gallery.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        instance.close();
+    }});
+}
+
+
+
